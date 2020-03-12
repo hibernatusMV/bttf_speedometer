dcb56b8529ec358b21f0b5348c0055e1e8d35168
Marcus <40230046+hibernatusMV@users.noreply.github.com>
2020-03-12 13:46:31 +0100

    Update bttf_speedometer.ino

--- bttf_speedometer.ino
+++ bttf_speedometer.ino
@@ -2,11 +2,10 @@
 #define CLK 3
 #define DATA 2
 
-// Array zur Ablage der Zahlencodierung für die LED-Beschaltung
-
 byte digit[10]= {0x6F, 0x09, 0x73, 0x3B, 0x1D, 0x3E, 0x7C, 0x0B, 0x7F, 0x1F};
 
 int i;
+int j;
 
 void setup(){
   pinMode(LATCH, OUTPUT);
@@ -18,9 +17,10 @@ void setup(){
 
 void loop(){
   for (i=1; i<=88; i++) {
-    if (i<=25) delay(150);
-    if ((i>25) && (i<=80)) delay(250);
-    if ((i>80) && (i<=88)) delay(500);
+    // Accelerate
+    if (i<=25) delay(100);
+    if ((i>25) && (i<=80)) delay(200);
+    if ((i>80) && (i<=88)) delay(400);
 
     int firstDigit = i/10;
     int secondDigit = i%10;
@@ -35,8 +35,25 @@ void loop(){
     }
     digitalWrite(LATCH, HIGH);
   }
+  
   if(i > 88) {
     delay(5000);
+    // Brake
+    for (j=88; j>=1; j--) {
+      delay(50);
+      int firstDigit = j/10;
+      int secondDigit = j%10;
+  
+      digitalWrite(LATCH, LOW);
+      shiftOut(DATA, CLK, MSBFIRST, ~digit[secondDigit]); // digitOne
+      
+      if (firstDigit > 0) {
+        shiftOut(DATA, CLK, MSBFIRST, ~digit[firstDigit]); // digitTwo 
+      } else {
+        shiftOut(DATA, CLK, MSBFIRST, B11111111); // digitOne
+      }
+      digitalWrite(LATCH, HIGH);
+    }
     initDisplay();
   }
 }
